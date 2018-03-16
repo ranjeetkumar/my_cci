39737b0321ba4025e19bb53c08d5d505d0720c47
Ranjeet Kumar <ranjeet.pesit@gmail.com>
2018-03-16 16:18:00 +0530

    solution to q8

---
+++ chapter2/8.py
@@ -0,0 +1,66 @@
+#Loop detection
+class Node(object):
+
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class SingleList(object):
+
+    head = None
+    tail = None
+    
+    def print(self):
+        x = self.head
+        while x is not None:
+            print(x.data,end='')
+            print(" -> ",end='')
+            x = x.next
+        print(None)
+
+    def add(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+
+    def __len__(self):
+        result = 0
+        node = self.head
+        while node:
+            result += 1
+            node = node.next
+        return result
+
+def loop_detection(ll):
+    fast = slow = ll.head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast is slow:
+            break
+
+    if fast is None or fast.next is None:
+        return None
+
+    slow = ll.head
+    while fast is not slow:
+        fast = fast.next
+        slow = slow.next
+
+    return fast
+    
+       
+    
+ 
+if __name__ == "__main__":
+    ll1 = SingleList()
+    ll1.add(7)
+    ll1.add(1)
+    ll1.add(6)
+    print (loop_detection(ll1))
